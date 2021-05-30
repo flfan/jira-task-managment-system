31b30ae2c0dafd526aeb271b4cdcfc510ccf29be
flfan <1490459938@qq.com>
2021-05-30 15:31:27 +0800

    style: optimize project-list page

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "utils";
 import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -25,9 +26,14 @@ export const ProjectListScreen = () => {
     client("users").then(setUsers);
   });
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,4 @@
-import { Input, Select } from "antd";
+import { Input, Select, Form } from "antd";
 
 export interface User {
   id: string;
@@ -20,11 +20,12 @@ interface SearchPanelProps {
 }
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
-    <form>
-      <div>
+    <Form style={{ marginBottom: "2rem" }} layout={"inline"}>
+      <Form.Item>
         {/* {setParam(Object.assign({}, param,{name:''}))} */}
         <Input
           type="text"
+          placeholder={"项目名"}
           value={param.name}
           onChange={(evt) =>
             setParam({
@@ -33,6 +34,8 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
             })
           }
         />
+      </Form.Item>
+      <Form.Item>
         <Select
           value={param.personId}
           onChange={(value) =>
@@ -49,7 +52,7 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
             </Select.Option>
           ))}
         </Select>
-      </div>
-    </form>
+      </Form.Item>
+    </Form>
   );
 };
