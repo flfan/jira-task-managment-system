8babfaa268cf49b92ae1f8d1505472dd94ad5130
flfan <1490459938@qq.com>
2021-09-30 17:10:55 +0800

    fix: test

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -7,39 +7,45 @@ import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
 import { Button, Dropdown, Menu } from "antd";
 
 export const AuthenticatedApp = () => {
-  const { logout, user } = useAuth();
   return (
     <div>
       <Container>
-        <Header between={true}>
-          <HeaderLeft gap={true}>
-            <SoftwareLogo width={"18rem"} color={"rgb(38,132,255)"} />
-            <h2>项目</h2>
-            <h2>用户</h2>
-          </HeaderLeft>
-          <HeaderRight>
-            <Dropdown
-              overlay={
-                <Menu>
-                  <Menu.Item key={"logout"}>
-                    <Button type={"link"} onClick={logout}>
-                      登出
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              }
-            >
-              <Button type={"link"} onClick={(e) => e.preventDefault()}>
-                Hi, {user?.name}
-              </Button>
-            </Dropdown>
-          </HeaderRight>
-        </Header>
+        <PageHeader />
         <Main>
           <ProjectListScreen />
         </Main>
       </Container>
     </div>
+  );
+};
+
+const PageHeader = () => {
+  const { logout, user } = useAuth();
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width={"18rem"} color={"rgb(38,132,255)"} />
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={"logout"}>
+                <Button type={"link"} onClick={logout}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type={"link"} onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
   );
 };
 
